0e334e0056b73f516f82ad798d8d33bca98f422b
chenhuxy <chenhu8906@126.com>
2021-03-30 16:20:36 +0800

    update docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-while true;do sleep 10;done
+
 python3 manage.py makemigrations && python3 manage.py migrate
+
 python3 -m celery worker -A myweb -P eventlet -l info &
+
 python3 manage.py runserver 0.0.0.0:8000
