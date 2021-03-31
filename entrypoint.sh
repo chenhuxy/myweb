90375467c8c8776eb15093843280596a72b12193
chenhuxy <chenhu8906@126.com>
2021-03-31 13:30:24 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-#python3 manage.py makemigrations && python3 manage.py migrate
 
-python3 -m celery worker -A myweb -P eventlet -l info &
-
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000 &
+python3 -m celery worker -A myweb -P eventlet -l info -f /myweb/logs/celery.log
