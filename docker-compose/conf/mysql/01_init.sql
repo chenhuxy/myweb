a305e670a594ec4107079b3602fd21d875655565
root <root@node1.example.com>
2023-08-24 10:47:31 +0800

    修复数据库名称和url path错误

--- docker-compose/conf/mysql/01_init.sql
+++ docker-compose/conf/mysql/01_init.sql
@@ -1,3 +1,3 @@
-create database if not exists `django_test` default character set utf8 collate utf8_general_ci;
+create database if not exists `myweb` default character set utf8 collate utf8_general_ci;
 grant all on myweb.* to myweb@'%' identified by 'myweb';
 flush privileges;
