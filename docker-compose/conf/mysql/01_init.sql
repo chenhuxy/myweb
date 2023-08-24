71dcd985e5af3b9d2c909605decb2dd10b66ad00
root <root@node1.example.com>
2023-08-24 10:33:18 +0800

    修改数据库名称

--- docker-compose/conf/mysql/01_init.sql
+++ docker-compose/conf/mysql/01_init.sql
@@ -1,3 +1,3 @@
 create database if not exists `django_test` default character set utf8 collate utf8_general_ci;
-grant all on django_test.* to myweb@'%' identified by 'myweb';
+grant all on myweb.* to myweb@'%' identified by 'myweb';
 flush privileges;
