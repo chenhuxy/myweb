5e70c68778836150f4eea89958950c2b029fab69
root <root@node1.example.com>
2023-08-24 09:35:53 +0800

    修复容器启动问题

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+python3 manage.py makemigrations
+
 python3 manage.py runserver 0.0.0.0:8000 >/myweb/logs/server.log &
 python3 -m celery worker -A myweb -P eventlet -l info -f /myweb/logs/celery.log
