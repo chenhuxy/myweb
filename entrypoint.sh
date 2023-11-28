9dcc64558409a4792c7df95b138e4a245f322578
root <root@node1.example.com>
2023-11-28 16:37:32 +0800

    1. 增加任务日志到stdout 2. 修复MySQL监控检查报错

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ python3 manage.py runserver 0.0.0.0:8000 &
 python3 -m celery worker -A myweb -P solo -n worker1 -l info -f /myweb/logs/celery.log &
 python3 -m celery worker -A myweb -P solo -n worker2 -l info -f /myweb/logs/celery.log &
 python3 -m celery worker -A myweb -P solo -n worker3 -l info -f /myweb/logs/celery.log &
-python3 -m celery worker -A myweb -P solo -n worker4 -l info -f /myweb/logs/celery.log
+python3 -m celery worker -A myweb -P solo -n worker4 -l info -f /myweb/logs/celery.log &
+tail -f /myweb/logs/celery.log
