602a3490afa1715db2c8d49528f18c428e97c0b5
root <root@node1.example.com>
2024-01-11 10:54:42 +0800

    清理一些文件

--- apps/app01/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class App01Config(AppConfig):
-    name = 'apps.app01'
-    verbose_name = '应用app01'
--- practice/paramiko_sftp.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# _*_ coding:utf-8 _*_
-import paramiko
-transport = paramiko.Transport('192.168.1.6',22)
-transport.connect(username='root',password='fd#11051045')
-sftp = paramiko.SFTPClient.from_transport(transport)
-sftp.put('D:\Program Files\Python36\LICENSE.txt','/root/licence')
-sftp.get('/root/','D:\新建文件夹')
-transport.close()
